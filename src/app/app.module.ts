c900f5544bc5ade6bf5038b26b3e34d036bbcef7
Abdou Med Abdeljelil <abdoumohamedabdeljelil@outlook.com>
2023-05-14 11:28:55 +0000

    Life Skills Training App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { AjoutComponent } from './ajout/ajout.component';
+import { ListComponent } from './list/list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AjoutComponent,
+    ListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
